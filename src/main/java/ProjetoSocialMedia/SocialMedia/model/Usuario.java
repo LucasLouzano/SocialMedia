1c514f6a5e25a64ac2b790a97d89db339079d9b6
LucasLouzano <lucas_louzano@hotmail.com>
2024-01-09 12:30:15 -0300

    Alteração na classe usuario

--- src/main/java/ProjetoSocialMedia/SocialMedia/model/Usuario.java
+++ src/main/java/ProjetoSocialMedia/SocialMedia/model/Usuario.java
@@ -1,8 +1,6 @@
 package ProjetoSocialMedia.SocialMedia.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,57 +8,32 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
+@Entity(name = "usuarios")
+@Table(name = "usuarios")
 public class Usuario implements UserDetails {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String login;
-    private String password;
-    private UsuarioRole role;
 
-    public Usuario(String login, String password, UsuarioRole role) {
-        this.login = login;
-        this.password = password;
-        this.role = role;
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String login;
+
+    private String password;
+
+    private String role;
+
+    public Usuario() {}
+
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public UsuarioRole getRole() {
-        return role;
-    }
-
-    public void setRole(UsuarioRole role) {
-        this.role = role;
-    }
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        if(this.role == UsuarioRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
-        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        return List.of(new SimpleGrantedAuthority(role));
     }
+
     @Override
     public String getPassword() {
         return password;
