da99f760a3a442ada97e3c8fc391586c4db263a0
LucasLouzano <lucas_louzano@hotmail.com>
2024-02-20 14:45:34 -0300

    api de configuração do swagger

--- src/main/java/ProjetoSocialMedia/SocialMedia/configuration/SwaggerConfig.java
+++ src/main/java/ProjetoSocialMedia/SocialMedia/configuration/SwaggerConfig.java
@@ -1,21 +1,34 @@
 package ProjetoSocialMedia.SocialMedia.configuration;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI customAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Projeto Social Media")
-                        .description("Um projeto de mídia social para interações online.")
-                        .version("1.0.0")
-                        .license(new License().name("Apache 2.0").url("https://github.com/LucasLouzano/SocialMedia")));
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("projetoSocialMedia.SocialMedia.api"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring boot Rest Api")
+                .description("Demo project for Spring Boot Social Media.")
+                .version("1.0.0")
+                .contact(new Contact("Lucas Louzano","https://github.com/LucasLouzano/SocialMedia", "lucaslouzano@55gmail.com"))
+                .build();
     }
 }
