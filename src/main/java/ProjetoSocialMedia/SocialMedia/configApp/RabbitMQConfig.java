8f2137b143b688646dc063109eeb01f2b0ba5b08
LucasLouzano <lucas_louzano@hotmail.com>
2024-06-03 12:58:30 -0300

    Ajuste na class RabbitMQConfig

--- src/main/java/ProjetoSocialMedia/SocialMedia/configApp/RabbitMQConfig.java
+++ src/main/java/ProjetoSocialMedia/SocialMedia/configApp/RabbitMQConfig.java
@@ -1,12 +1,10 @@
 package ProjetoSocialMedia.SocialMedia.configApp;
 
-
-import lombok.Value;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Queue;
 
 @Configuration
 public class RabbitMQConfig {
