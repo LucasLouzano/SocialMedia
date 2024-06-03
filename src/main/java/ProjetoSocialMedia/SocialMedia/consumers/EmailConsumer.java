414f2e946cea385e0cc6d348119259f85661b47d
LucasLouzano <lucas_louzano@hotmail.com>
2024-06-03 12:56:03 -0300

    Implementação da class emailConsumer

---
+++ src/main/java/ProjetoSocialMedia/SocialMedia/consumers/EmailConsumer.java
@@ -0,0 +1,24 @@
+package ProjetoSocialMedia.SocialMedia.consumers;
+import ProjetoSocialMedia.SocialMedia.dto.EmailDto;
+import ProjetoSocialMedia.SocialMedia.model.EmailModel;
+import ProjetoSocialMedia.SocialMedia.service.impl.EmailService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailConsumer {
+
+    @Autowired
+   private EmailService emailService;
+
+    @RabbitListener(queues = "${spring.rabbitmq.queue}")
+    public void listen(@Payload EmailDto emailDto) {
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        System.out.println("Email Status: " + emailModel.getStatusEmail().toString());
+    }
+}
