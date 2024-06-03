b6b9093b18392f926aa329c6c885bc85aad29352
LucasLouzano <lucas_louzano@hotmail.com>
2024-06-03 12:37:43 -0300

    Implementação do método no endpoint getAllEmails

--- src/main/java/ProjetoSocialMedia/SocialMedia/controller/EmailController.java
+++ src/main/java/ProjetoSocialMedia/SocialMedia/controller/EmailController.java
@@ -7,25 +7,36 @@ import ProjetoSocialMedia.SocialMedia.model.EmailModel;
 import ProjetoSocialMedia.SocialMedia.service.impl.EmailService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-    @RestController
-    public class EmailController {
-        @Autowired
-        private EmailService emailService;
-        @Autowired
-        private EmailMapper emailMapper;
+@RestController
+public class EmailController {
+    @Autowired
+    private EmailService emailService;
+    @Autowired
+    private EmailMapper emailMapper;
 
-        @PostMapping("/sending-email")
-        public ResponseEntity<EmailDTOInfo> sendingEmail(@RequestBody @Valid EmailModel emailModel) {
-            EmailDto emailDto = emailService.sendEmail(emailModel);
-            EmailDTOInfo emailDTOInfo = emailMapper.mapEmailToDto(emailDto);
-            return new ResponseEntity<>(emailDTOInfo, HttpStatus.CREATED);
-        }
+    @PostMapping("/sending-email")
+    public ResponseEntity<EmailDTOInfo> sendingEmail(@RequestBody @Valid EmailModel emailModel) {
+        EmailDto emailDto = emailService.sendEmail(emailModel);
+        EmailDTOInfo emailDTOInfo = emailMapper.mapEmailToDto(emailDto);
+        return new ResponseEntity<>(emailDTOInfo, HttpStatus.CREATED);
     }
+
+    @GetMapping("/emails")
+    public ResponseEntity<Page<EmailModel>> getAllEmails(@PageableDefault(page = 0, size = 5, sort = "emailId", direction = Sort.Direction.DESC) Pageable pageable) {
+        return new ResponseEntity<>(emailService.findAll(pageable), HttpStatus.OK);
+    }
+
+}
 
 
