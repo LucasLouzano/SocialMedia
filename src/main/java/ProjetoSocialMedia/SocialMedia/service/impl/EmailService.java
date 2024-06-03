26c196830e68843400c5ba92052b7280606e2612
LucasLouzano <lucas_louzano@hotmail.com>
2024-06-03 12:35:25 -0300

    Implementação do findall na class de serviço

--- src/main/java/ProjetoSocialMedia/SocialMedia/service/impl/EmailService.java
+++ src/main/java/ProjetoSocialMedia/SocialMedia/service/impl/EmailService.java
@@ -6,9 +6,11 @@ import ProjetoSocialMedia.SocialMedia.mapper.EmailMapper;
 import ProjetoSocialMedia.SocialMedia.model.EmailModel;
 import ProjetoSocialMedia.SocialMedia.repository.EmailRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -44,6 +46,10 @@ public class EmailService {
         }
         EmailModel emailModel = repository.save(email);
         return mapper.emailModelToEmailDto(emailModel);
-
     }
+
+    public Page<EmailModel> findAll(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
 }
