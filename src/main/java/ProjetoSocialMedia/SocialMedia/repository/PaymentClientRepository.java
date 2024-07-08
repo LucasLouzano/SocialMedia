bbe17612be9d89fec66919f0b996643a239be668
LucasLouzano <lucas_louzano@hotmail.com>
2024-07-08 17:58:50 -0300

    Implementação da interface PaymentClientRepository

--- src/main/java/ProjetoSocialMedia/SocialMedia/repository/PaymentClientRepository.java
+++ src/main/java/ProjetoSocialMedia/SocialMedia/repository/PaymentClientRepository.java
@@ -3,14 +3,15 @@ package ProjetoSocialMedia.SocialMedia.repository;
 import ProjetoSocialMedia.SocialMedia.model.PaymentClient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public interface PaymentClientRepository extends JpaRepository<PaymentClient,Long> {
+public interface PaymentClientRepository extends JpaRepository<PaymentClient, Long> {
     @Query("SELECT p FROM PaymentClient p WHERE p.mes BETWEEN :startDate AND :endDate")
-    List<PaymentClient> findBetweenDates(Date startDate, Date endDate);
+    List<PaymentClient> findBetweenDates(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 
 }
