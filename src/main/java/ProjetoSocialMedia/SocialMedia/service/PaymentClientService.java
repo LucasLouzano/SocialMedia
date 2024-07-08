8189e82cebf88b9783e2777e1e416a262016e216
LucasLouzano <lucas_louzano@hotmail.com>
2024-07-08 18:00:08 -0300

    Implementação da interface PaymentClientService

--- src/main/java/ProjetoSocialMedia/SocialMedia/service/PaymentClientService.java
+++ src/main/java/ProjetoSocialMedia/SocialMedia/service/PaymentClientService.java
@@ -1,14 +1,14 @@
 package ProjetoSocialMedia.SocialMedia.service;
-
 import ProjetoSocialMedia.SocialMedia.model.PaymentClient;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PaymentClientService {
 
     PaymentClient save(PaymentClient paymentClient);
     List<PaymentClient> findAll();
-    List<PaymentClient> findBetweenDates(Date startDate, Date endDate);
+    List<PaymentClient> findBetweenDates(LocalDateTime startDateTime, LocalDateTime endDateTime);
     Boolean paid();
+
 }
