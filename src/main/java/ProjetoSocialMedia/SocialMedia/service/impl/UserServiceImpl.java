7729126dbf91b1b2757cf416d885b0911a5c8a61
LucasLouzano <lucas_louzano@hotmail.com>
2024-07-15 13:41:44 -0300

    Implementação do UserServiceImpÃl

--- src/main/java/ProjetoSocialMedia/SocialMedia/service/impl/UserServiceImpl.java
+++ src/main/java/ProjetoSocialMedia/SocialMedia/service/impl/UserServiceImpl.java
@@ -1,2 +1,20 @@
-package ProjetoSocialMedia.SocialMedia.service.impl;public class UserServiceImpl {
+package ProjetoSocialMedia.SocialMedia.service.impl;
+
+import ProjetoSocialMedia.SocialMedia.model.Usuario;
+import ProjetoSocialMedia.SocialMedia.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public Usuario getUsuario() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails){
+            return (Usuario)((UserDetails)principal);
+        }else{
+            return null;
+        }
+    }
 }
