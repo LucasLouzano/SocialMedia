410b1b8744e8618d5f382cc443b89eadb76dc824
LucasLouzano <lucas_louzano@hotmail.com>
2024-07-22 18:07:10 -0300

    Implementação do DTO

--- src/main/java/ProjetoSocialMedia/SocialMedia/dto/EmailDTO.java
+++ src/main/java/ProjetoSocialMedia/SocialMedia/dto/EmailDTO.java
@@ -1,2 +1,4 @@
-package ProjetoSocialMedia.SocialMedia.dto;public record EmailDTO() {
+package ProjetoSocialMedia.SocialMedia.dto;
+
+public record EmailDTO(String ownerRef,String emailFrom,String emailTo,String subject,String text) {
 }
