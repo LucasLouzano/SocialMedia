8e568a10d8697e31fc603d058ebc45f938bd07fe
LucasLouzano <lucas_louzano@hotmail.com>
2024-07-24 20:25:10 -0300

    Ajuste no PostApiConfiguration

--- src/main/java/ProjetoSocialMedia/SocialMedia/configApp/PostApiConfiguration.java
+++ src/main/java/ProjetoSocialMedia/SocialMedia/configApp/PostApiConfiguration.java
@@ -1,6 +1,8 @@
 package ProjetoSocialMedia.SocialMedia.configApp;
 
+import ProjetoSocialMedia.SocialMedia.mapper.PostsMapper;
 import com.squareup.okhttp.OkHttpClient;
+import org.mapstruct.factory.Mappers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,9 +13,12 @@ public class PostApiConfiguration {
     public OkHttpClient okHttpClient() {
         return new OkHttpClient();
     }
-
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+    @Bean
+    public PostsMapper postsMapper() {
+        return Mappers.getMapper(PostsMapper.class);
     }
 }
